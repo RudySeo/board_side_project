4daf080065955802e16554d0c8d1e31552fa3b85
RudySeo <gun100491@gmail.com>
2023-08-14 20:48:45 +0900

    Feat:Entity comment 추가

---
+++ src/main/java/sideproject/board/model/entity/Comment.java
@@ -0,0 +1,34 @@
+package sideproject.board.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String content;
+
+
+    @CreatedDate
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    private LocalDateTime modified_at;
+}
