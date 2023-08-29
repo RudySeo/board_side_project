f6145dafb5caecd01c3b6a6fe9cdce845647150f
RudySeo <gun100491@gmail.com>
2023-08-29 15:57:24 +0900

    Feat:Entity memberId 삭제

--- src/main/java/sideproject/board/member/entity.java
+++ src/main/java/sideproject/board/member/entity.java
@@ -1,4 +1,34 @@
 package sideproject.board.member;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class entity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String email;
+	private String password;
+	private String name;
+	private int age;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime modifiedAt;
 }
