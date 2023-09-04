af4d9da286f55b22a70796b53823040885573feb
RudySeo <gun100491@gmail.com>
2023-09-04 17:16:29 +0900

    Feat:모든파일 삭제

--- src/main/java/sideproject/board/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package sideproject.board.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(Long id) {
-        super("Error message" + id);
-    }
-}
--- src/main/java/sideproject/board/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package sideproject.board.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sideproject.board.model.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-}
