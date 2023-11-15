f7d13e9d54941f315fa0bac462c07b0c05f06428
RudySeo <gun100491@gmail.com>
2023-11-15 17:32:02 +0900

    Fix: 주석 삭제

--- src/main/java/sideproject/board/point/contoller/PointHistoryController.java
+++ src/main/java/sideproject/board/point/contoller/PointHistoryController.java
@@ -57,22 +57,6 @@ public class PointHistoryController {
 		return new PageImpl<>(responses, pageable, pointPage.getTotalElements());
 	}
 
-	// @GetMapping("/point")
-	// public Page<PointHistoryResponse> searchPointList(Pageable pageable) {
-	//
-	// 	Member member = ThreadLocalContext.get();
-	//
-	// 	Page<PointHistory> pointPage = pointHistoryService.searchPointList(member, pageable);
-	//
-	// 	List<PointHistoryResponse> responses = pointPage.getContent()
-	// 		.stream()
-	// 		.map(i -> new PointHistoryResponse(i.getMember().getName(), i.getMember().getMoney(), i.getAmount(),
-	// 			i.getChargeTime()))
-	// 		.collect(Collectors.toList());
-	//
-	// 	return new PageImpl<>(responses, pageable, pointPage.getTotalElements());
-	// }
-
 
 	@GetMapping("/point/{id}")
 	public PointHistoryResponse searchUserPoint(@PathVariable Long id) {
