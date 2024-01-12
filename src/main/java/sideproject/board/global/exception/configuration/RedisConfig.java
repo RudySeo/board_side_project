4e28efd439b6e5a4207e6c14233689d64bc379b4
RudySeo <gun100491@gmail.com>
2024-01-12 15:10:35 +0900

    Feat: 레디스 캐시 설정

--- src/main/java/sideproject/board/global/exception/configuration/RedisConfig.java
+++ src/main/java/sideproject/board/global/exception/configuration/RedisConfig.java
@@ -1,12 +1,19 @@
 package sideproject.board.global.exception.configuration;
 
+import java.time.Duration;
+
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import lombok.RequiredArgsConstructor;
@@ -32,5 +39,18 @@ public class RedisConfig {
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 
 		return redisTemplate;
+	}
+
+	@Bean
+	public CacheManager contentCacheManager(RedisConnectionFactory cf) {
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+				new GenericJackson2JsonRedisSerializer())) // Value Serializer 변경
+			.entryTtl(Duration.ofMinutes(3L)); // 캐시 수명 30분
+
+		return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf)
+			.cacheDefaults(redisCacheConfiguration)
+			.build();
 	}
 }
