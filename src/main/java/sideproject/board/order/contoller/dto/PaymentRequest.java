db7e085d695f06cc4127c233c2aa78ee9a29f686
RudySeo <gun100491@gmail.com>
2024-07-25 19:07:38 +0900

    Feat: 생성자 추가

--- src/main/java/sideproject/board/order/contoller/dto/PaymentRequest.java
+++ src/main/java/sideproject/board/order/contoller/dto/PaymentRequest.java
@@ -1,8 +1,10 @@
 package sideproject.board.order.contoller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PaymentRequest {
 
 	private Long productId;
