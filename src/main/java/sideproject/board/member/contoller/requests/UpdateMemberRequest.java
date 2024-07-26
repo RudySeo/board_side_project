5ee929c59a2523904906bf23f4c11d18a45ccfa2
RudySeo <gun100491@gmail.com>
2024-07-26 17:03:14 +0900

    Fix: Dto 수정

--- src/main/java/sideproject/board/member/contoller/requests/UpdateMemberRequest.java
+++ src/main/java/sideproject/board/member/contoller/requests/UpdateMemberRequest.java
@@ -5,26 +5,18 @@ import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class UpdateMemberRequest {
-	private Long id;
+
 	@NotNull
 	private String name;
+	
 	@NotNull
 	private int age;
 
-	// public static UpdateMemberRequest convertToUpdateMemberReq(MemberEntity MemberEntity) {
-	// 	return UpdateMemberRequest.builder()
-	// 		.id(MemberEntity.getId())
-	// 		.name(MemberEntity.getName())
-	// 		.age(MemberEntity.getAge())
-	// 		.build();
-	// }
 }
 
 
